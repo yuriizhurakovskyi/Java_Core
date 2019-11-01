8d2434e20b9c802e782273ca76123f96f55bf605
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2019-11-01 23:36:18 +0200

    homework 9 lesson 11 Arrays

---
+++ src/lgscourse/javacore/lesson11/CarsArrays/Car.java
@@ -0,0 +1,42 @@
+package lgscourse.javacore.lesson11.CarsArrays;
+
+public class Car {
+	private int horsepower;
+	private int year;
+	SteeringWheel steeringWheel;
+	Engine engine;
+	
+		
+	public Car(int horsepower, int year, SteeringWheel steeringWheel, Engine engine) {
+		this.horsepower = horsepower;
+		this.year = year;
+		this.steeringWheel = steeringWheel;
+		this.engine = engine;
+	}
+	
+	public int getHorsepower() {
+		return horsepower;
+	}
+	
+	
+	public int getYear() {
+		return year;
+	}
+
+
+	public SteeringWheel getSteeringWheel() {
+		return steeringWheel;
+	}
+
+
+	public Engine getEngine() {
+		return engine;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "\nCar : horsepower = " + horsepower + ", year of manufacturing = " + year + "\n    " + steeringWheel + "\n    " + engine;
+	}
+	
+}
---
+++ src/lgscourse/javacore/lesson11/CarsArrays/SteeringWheel.java
@@ -0,0 +1,23 @@
+package lgscourse.javacore.lesson11.CarsArrays;
+
+public class SteeringWheel {
+	private int diameter;
+	private Material material;
+	public SteeringWheel(int diameter, Material material) {
+		this.diameter = diameter;
+		this.material = material;
+	}
+	
+	public int getDiameter() {
+		return diameter;
+	}
+	public Material getMaterial() {
+		return material;
+	}
+	
+	@Override
+	public String toString() {
+		return "Steering wheel : diameter = " + diameter + ", material = " + material.name();
+	}
+	
+}
