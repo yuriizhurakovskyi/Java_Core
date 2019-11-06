5770b25bce173b110c7b28ddd5ba678b13ddeddb
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2019-11-06 13:16:09 +0200

    homework 12 lesson 14 Sets, Comparators

---
+++ src/lgscourse/javacore/lesson14/BookComparator/BookAllFieldsComparator.java
@@ -0,0 +1,20 @@
+package lgscourse.javacore.lesson14.BookComparator;
+
+import java.util.Comparator;
+
+public class BookAllFieldsComparator implements Comparator<Book> {
+
+	@Override
+	public int compare(Book o1, Book o2) {
+		if(o1.getPages() > o2.getPages())
+			return 1;
+		else if (o1.getPages() < o2.getPages())
+			return -1;
+		else if(o1.getName().compareTo(o2.getName()) > 0)
+			return 1;
+		else if(o1.getName().compareTo(o2.getName()) < 0)
+			return -1;
+		return 0;
+	}
+
+}
